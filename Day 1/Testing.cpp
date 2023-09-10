0e186931900675376cdf8223a43b6558f1ba785a
acidentally <iCombozstrafe@gmail.com>
2023-09-10 10:20:01 +0700

    huh

---
+++ Day 1/Testing.cpp
@@ -0,0 +1,55 @@
+/*
+Good luck for those who are trying your best
+May the most glorious victory come
+File name: Testing.cpp
+Code by : acident / lckintrovert
+Created since : 09/09/2023 ~~ 20:09:06
+Literally the worst cp-er ever
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int                 long long
+#define uint                unsigned long long
+#define dub                 double
+#define fi                  first
+#define se                  second
+#define endl                '\n'
+#define pb                  push_back
+#define pf                  push_front
+#define eb                  emplace_back
+#define ins                 insert
+#define mp                  make_pair
+#define all(a)              a.begin(), a.end()
+#define YES                 cout << "YES\n"
+#define NO                  cout << "NO\n"
+#define coutdub(x)          cout << fixed << setprecision(x)
+#define cerrdub(x)          cerr << fixed << setprecision(x)
+ 
+//#define _USE_MATH_DEFINES // If meth .__.
+ 
+template<class T1, class T2> void maximize(T1& a, T2 b) {a = max(a, b);}
+template<class T1, class T2> void minimize(T1& a, T2 b) {a = min(a, b);}
+ 
+typedef vector<int>         vi;
+typedef vector<vi>          vvi;
+typedef pair<int, int>      pi;
+typedef pair<int, pi>       pii;
+int const mod       =       1e9 + 7;
+int const maxn      =       1e5 + 10;
+int const INF       =       1e18;
+ 
+
+void solve() {
+    
+}
+signed main() {
+    ios_base:: sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    //File?
+    solve();
+}
+
+/*A place to scribble thoughts
+
+*/
