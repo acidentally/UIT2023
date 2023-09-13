66f80ed596efc836d287c71114fc585736bacf99
acidentally <iCombozstrafe@gmail.com>
2023-09-13 09:11:34 +0700

    lck bi ngu

--- Day 1/E.cpp
+++ Day 1/E.cpp
@@ -3,7 +3,7 @@ Good luck for those who are trying your best
 May the most glorious victory come
 File name: E.cpp
 Code by : acident / lckintrovert
-Created since : 12/09/2023 ~~ 17:40:10
+Created since : 13/09/2023 ~~ 08:11:59
 Literally the worst cp-er ever
 */
 #include <bits/stdc++.h>
@@ -36,18 +36,79 @@ typedef vector<int>         vi;
 typedef vector<vi>          vvi;
 typedef pair<int, int>      pi;
 typedef pair<int, pi>       pii;
-int const mod       =       1e9 + 7;
-int const maxn      =       1e5 + 10;
+int const mod       =       998244353;
+int const maxn      =       2e5 + 10;
 int const INF       =       1e18;
  
+int n, m, ans = 0;
+int C[5][maxn] = {};
+vi a[maxn] = {}, query[maxn] = {};
+void init() {
+    for(int u = 1; u <= n; u++) {
+        if(a[u].size() < 4) continue;
+        for(auto v : a[u]) {
+            if(a[v].size() < 3) continue;
+            if(a[u].size() > a[v].size() || (a[u].size() == a[v].size() && u > v)) {
+                query[u].pb(v);
+            }
+        }
+    }
+    for(int i = 0; i <= n; i++) {
+        C[0][i] = 1;
+        if(i == 0) continue;
+        C[1][i] = i;
+        if(i >= 2) C[2][i] = (i * (i - 1) / 2) % mod;
+        if(i >= 3) C[3][i] = (i * (i - 1) / 2 * (i - 2) / 3) % mod;
+    }
+}
+bool check[maxn] = {};
+int intersec, u_sat, v_sat;
+void compute() {
+    for(int u = 1; u <= n; u++) {
+        for(auto it : a[u]) check[it] = true;
 
+        for(auto v : query[u]) {
+            intersec = 0;
+            for(auto v_ : a[v]) if(check[v_]) intersec++;
+            u_sat = a[u].size() - intersec - 1;
+            v_sat = a[v].size() - intersec - 1;
+
+            int times = 2;
+            while(times--) {
+                for(int x = 0; x < 4; x++) {
+                    for(int y = 0; y < 3; y++) {
+                        if((x + y <= intersec) && (3 - x <= u_sat) && (2 - y <= v_sat)) {
+                            (ans += (1ll * C[3 - x][u_sat] % mod * C[2 - y][v_sat] % mod) *
+                                   (C[x][intersec] % mod * C[y][intersec - x] % mod) % mod) %= mod;
+                        }
+                    }
+                }
+                swap(u_sat, v_sat);
+            }
+        }
+
+        for(auto v : a[u]) check[v] = false;
+    }
+}
+int u, v;
 void solve() {
-    
+    cin >> n >> m;
+    while(m--) {
+        cin >> u >> v;
+        a[u].pb(v);
+        a[v].pb(u);
+    }
+    init();
+    compute();
+    cout << (ans % mod) << endl;
 }
 signed main() {
     ios_base:: sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
-    //File?
+    #ifdef ONLINE_JUDGE
+    freopen("men-graph.INP", "r", stdin);
+    freopen("men-graph.OUT", "w", stdout);
+    #endif //ONLINE JUDGE
     solve();
 }
 
