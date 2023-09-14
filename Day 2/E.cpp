166ee4b8fdd1b91f37e264adf3c62e0e58d9595a
acidentally <iCombozstrafe@gmail.com>
2023-09-14 18:02:20 +0700

    chua cuu lck

--- Day 2/E.cpp
+++ Day 2/E.cpp
@@ -9,7 +9,6 @@ Literally the worst cp-er ever
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int                 long long
 #define uint                unsigned long long
 #define dub                 double
 #define fi                  first
@@ -36,56 +35,58 @@ typedef vector<int>         vi;
 typedef vector<vi>          vvi;
 typedef pair<int, int>      pi;
 typedef pair<int, pi>       pii;
-int const mod       =       1e9 + 7;
-int const maxn      =       700;
-int const INF       =       1e18;
+int const maxn      =       1024;
+int const INF       =       300;
 
-struct make {
-    int i, a, b;
-    make() = default;
-    make(int i_, int a_, int b_) : i(i_), a(a_), b(b_) {}
-};
+bitset<1024> ok[202][1024] = {};
 
-bool check[210][650][650] = {};
-make TRACE[210][650][650];
-#define ok(i, a, b)         check[i][a][b]
-#define trace(i, a, b)      TRACE[i][a][b]
+int n, v[202] = {}, A, B, C, curA, curB, curC, sum = 0, ifA, ifB, ifC;
+char couting[202] = {};
 
-int n;
-int v[maxn] = {};
-char couting[maxn] = {};
+inline bool better(int x, int y, int z) {
+    if(x + y + z == A + B + C) 
+    return (((x > 0) + (y > 0) + (z > 0)) > ((A > 0) + (B > 0) + (C > 0)));
+    return (x + y + z > A + B + C);
+}
 inline void solve() {
-    if(n == 0) return;
+    if(n == 0) exit(0);
+    A = 0, B = 0;
+    curA = 0, curB = 0, curC = 0;
+    sum = 0;
     for(int i = 1; i <= n; i++) {
         cin >> v[i];
+        sum ^= v[i];
     }
-    ok(0, 0, 0) = 1;
+    memset(ok, 0, sizeof(ok));
+    C = sum;
+    ok[0][0][0] = 1;
     for(int i = 1; i <= n; i++) {
-        for(int a = 0; a <= 600; a++) {
-            for(int b = 0; b <= 600; b++) {
-                if(ok(i - 1, a, b)) {
-                    ok(i, a, b) = true;
-                    ok(i, a ^ v[i], b) = true;
-                    ok(i, a, b ^ v[i]) = true;
-
-                    trace(i, a, b) = make(i - 1, a, b);
-                    trace(i, a ^ v[i], b) = make(i - 1, a, b);
-                    trace(i, a, b ^ v[i]) = make(i - 1, a, b);
+        for(int a = 0; a < 1024; a++) {
+            for(int b = 0; b < 1024; b++) {
+                if(ok[i - 1][a][b]) {
+                    ok[i][a][b] = true;
+                    ok[i][a ^ v[i]][b] = true;
+                    ok[i][a][b ^ v[i]] = true;
+                    if(better(a, b, sum ^ a ^ b)) {
+                        A = a; B = b; C = sum ^ a ^ b;
+                    }
                 }
             }
         }
     }
-    int sum = 0, ans = 0;
-    make ending;
-    for(int i = 1; i <= n; i++) sum ^= v[i];
-    for(int a = 0; a <= 600; a++) for(int b = 0; b <= 600; b++) {
-        if(ok(n, a, b)) {
-            if(maximize(ans, a + b + sum ^ a ^ b)) {
-                ending = make(n, a, b);
-            }
-        }
+    for(int i = n; i >= 1; i--) {
+        ifA = ((ok[i - 1][A ^ v[i]][B]) ? curA : INF);
+        ifB = ((ok[i - 1][A][B ^ v[i]]) ? curB : INF);
+        ifC = ((ok[i - 1][A][B]) ? curC : INF);
+
+        if(ifA <= ifB && ifA <= ifC) {curA++; A ^= v[i]; couting[i] = 'P';}
+        else if(ifB <= ifC) {curB++; B ^= v[i]; couting[i] = 'V';}
+        else {curC++; couting[i] = 'H';};
     }
-    cout << ans << endl;
+    
+    int i = 1;
+    while(i <= n) cout << couting[i++];
+    cout << endl;
 }
 signed main() {
     ios_base:: sync_with_stdio(0);
@@ -95,8 +96,11 @@ signed main() {
     freopen("chemistry.OUT", "w", stdout);
     #endif //ONLINE JUDGE
 
-    int subtask; cin >> subtask;
-    solve();
+    cin >> n;
+    while(cin >> n) {
+        if(n == 0) exit(0);
+        solve();
+    }
 }
 
 /*A place to scribble thoughts
